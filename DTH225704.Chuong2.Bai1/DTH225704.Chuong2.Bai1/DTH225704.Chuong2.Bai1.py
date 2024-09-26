d36d0c0c3bf2f35dfa04f01a3c2c55f46a0657cd
HNTN-3107 <huynhnghia31072004@gmail.com>
2024-09-26 15:44:06 +0700

    Add files via upload

---
+++ DTH225704.Chuong2.Bai1/DTH225704.Chuong2.Bai1/DTH225704.Chuong2.Bai1.py
@@ -0,0 +1,10 @@
+﻿import math
+try:
+ r=float(input("Mời bạn nhập bán kính hình tròn:"))
+ cv=2*math.pi*r
+ dt=r**2*math.pi
+ formatcv= format(cv,".2f")
+ print("Chu vi = ",formatcv)
+ print("Diện tích=",dt)
+except:
+ print("Lỗi rồi!")
